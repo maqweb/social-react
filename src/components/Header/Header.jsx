171d7dda73f3cea7e635e70c18b54eca4d6e6613
maqweb <balikhinv@bk.ru>
2019-11-01 22:30:02 +0300

    create moduls css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className="header">
-      <img className="logo" src="https://www.clker.com/cliparts/N/0/4/q/4/R/react-redux.svg" alt="" />
+    <header className={style.header}>
+      <img className={style.logo} src="https://www.clker.com/cliparts/N/0/4/q/4/R/react-redux.svg" alt="" />
     </header>
   )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className="content">
-      <img className="img" src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+    <div className={style.content}>
+      <img className={style.img} src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
       <div>
         ava + description
         </div>
