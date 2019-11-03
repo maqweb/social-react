4454b4a1fe74704ca496ae6ebd74c9086fb97378
maqweb <balikhinv@bk.ru>
2019-11-03 23:06:03 +0300

    create component Dialogs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
 
-function App() {
+const App = () => {
   return (
     <div className="mainWrap">
-      <Header />
-      <Navbar />
-      <Profile />
+      <Header/>
+      <Navbar/>
+      <div className="content">
+        {/*<Profile/>*/}
+        <Dialogs/>
+      </div>
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,12 +4,14 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return (
-    <div className={style.content}>
-      <img className={style.img} src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+    <div>
+      <img className={style.img}
+           src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+           alt=""/>
       <div>
         ava + description
       </div>
-      <MyPosts />
+      <MyPosts/>
     </div>
   )
 }
