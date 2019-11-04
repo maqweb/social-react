565fc8086c1b159d37747de6d52f9b79eaccac30
maqweb <balikhinv@bk.ru>
2019-11-04 21:52:10 +0300

    added NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import style from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={style.nav}>
-      <div>
-        <a href="/profile">Profile</a>
+      <div className={style.item}>
+        <NavLink className={style.itemLink} activeClassName={style.activeLink} to="/profile">Profile</NavLink>
       </div>
-      <div>
-        <a href="/dialogs">Messages</a>
+      <div className={style.item}>
+        <NavLink className={style.itemLink} activeClassName={style.activeLink} to="/dialogs">Messages</NavLink>
       </div>
-      <div>
-        <a href="/news">News</a>
+      <div className={style.item}>
+        <NavLink className={style.itemLink} activeClassName={style.activeLink} to="/news">News</NavLink>
       </div>
-      <div>
-        <a href="/music">Music</a>
+      <div className={style.item}>
+        <NavLink className={style.itemLink} activeClassName={style.activeLink} to="/music">Music</NavLink>
       </div>
-      <div>
-        <a href="/settings">Settings</a>
+      <div className={style.item}>
+        <NavLink className={style.itemLink} activeClassName={style.activeLink} to="/settings">Settings</NavLink>
       </div>
     </nav>
   )
-}
+};
 
 export default Navbar;
