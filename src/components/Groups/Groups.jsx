4f6d2fec0530ce8d4bd5900d13d21cd3faf8dc40
maqweb <balikhinv@bk.ru>
2019-11-06 10:49:01 +0300

    add groups

--- src/components/Groups/Groups.jsx
+++ src/components/Groups/Groups.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-// import style from './Groups.module.css';
-
-const Groups = () => {
-    return (
-        <div>
-            Groups
-        </div>
-    )
-};
-
-export default Groups;
+import React from 'react';// import style from './Groups.module.css';const Groups = () => {    return (        <div>            Groups        </div>    )};export default Groups;
