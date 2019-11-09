774c636f309694ee890e3c285102af20470e046d
maqweb <balikhinv@bk.ru>
2019-11-09 22:11:25 +0300

    add render.js / добавление поста на стену

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,6 @@ const Header = () => {
       <img className={style.logo} src="https://www.clker.com/cliparts/N/0/4/q/4/R/react-redux.svg" alt="" />
     </header>
   )
-}
+};
 
 export default Header;
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,45 +1,46 @@
+import {rerenderEntireTree} from "../render";
+
 let state = {
     profilePage: {
         posts: [
-            { id: 1, message: 'Hello React!', likesCount: 23 },
-            { id: 1, message: 'It\'s nice work', likesCount: 11 },
-            { id: 1, message: 'Awesome!', likesCount: 7 }
+            {id: 1, message: 'Hello React!', likesCount: 23},
+            {id: 1, message: 'It\'s nice work', likesCount: 11},
+            {id: 1, message: 'Awesome!', likesCount: 7}
         ]
     },
 
     dialogPage: {
         dialogsData: [
-            { id: 1, name: 'Empty'},
-            { id: 2, name: 'Asdf'},
-            { id: 3, name: 'Qwer'},
-            { id: 4, name: 'Qaz'}
+            {id: 1, name: 'Empty'},
+            {id: 2, name: 'Asdf'},
+            {id: 3, name: 'Qwer'},
+            {id: 4, name: 'Qaz'}
         ],
         messagesData: [
-            { id: 1, message: 'Hello React' },
-            { id: 2, message: 'Nice work!' },
-            { id: 3, message: 'Let\'s do it!' }
+            {id: 1, message: 'Hello React'},
+            {id: 2, message: 'Nice work!'},
+            {id: 3, message: 'Let\'s do it!'}
         ]
     },
 
     navBar: {
         friends: [
-            { firstName: 'Empty', secondName: 'Jhon' },
-            { firstName: 'Marry', secondName: 'Marry' },
-            { firstName: 'Empty', secondName: 'Bill' },
-            { firstName: 'Clint', secondName: 'Eastwood' }
+            {firstName: 'Empty', secondName: 'Jhon'},
+            {firstName: 'Marry', secondName: 'Marry'},
+            {firstName: 'Empty', secondName: 'Bill'},
+            {firstName: 'Clint', secondName: 'Eastwood'}
         ]
     }
 };
 
 export let addPost = (postMessage) => {
-    debugger;
     let newPost = {
         id: 5,
         message: postMessage,
         likesCount: 0
     };
     state.profilePage.posts.push(newPost);
-    console.log(state.profilePage);
+    rerenderEntireTree(state);
 };
 
-export default  state;
+export default state;
