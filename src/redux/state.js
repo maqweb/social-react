f3cd9e3e9f5d39ce67de42d1b1cd693a5cfdf830
maqweb <balikhinv@bk.ru>
2019-11-11 22:57:02 +0300

    add callback, observer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,7 @@
-import {rerenderEntireTree} from "../render";
+// import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log('state changed');
+};
 
 let state = {
     profilePage: {
@@ -36,7 +39,7 @@ let state = {
 
 window.state = state;
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -47,9 +50,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 };
 
 export default state;
