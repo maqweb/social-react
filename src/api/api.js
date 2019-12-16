9d4bef33cbe8485517dff6285b4cf8d0c54c9225
maqweb <balikhinv@bk.ru>
2019-12-16 23:39:56 +0300

    add thunk 2

--- src/api/api.js
+++ src/api/api.js
@@ -11,21 +11,30 @@ const instance = axios.create({
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-            .then(response => {
-                return response.data
-            });
+                .then(response => {
+                    return response.data
+                });
     },
     unfollow(id) {
         return instance.delete(`follow/${id}`)
-            .then(response => {
-                return response.data
-            });
+                .then(response => {
+                    return response.data
+                });
     },
     follow(id) {
         return instance.post(`follow/${id}`)
-            .then(response => {
-                return response.data
-            });
+                .then(response => {
+                    return response.data
+                });
+    },
+    getProfile(userId) {
+        return instance.get(`profile/${userId}`)
+    }
+};
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
     }
 };
 
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,3 +1,5 @@
+import {authAPI} from "../api/api";
+
 const SET_USER_DATA = 'SET-USER-DATA';
 
 let initialState = {
@@ -23,5 +25,14 @@ const authReducer = (state = initialState, action) => {
 };
 
 export const setAuthUserData = (usersId, email, login) => ({type: SET_USER_DATA, data: { usersId, email, login }});
+
+export const getAuthUserData = () => (dispatch) => {
+    authAPI.me().then(response => {
+        if (response.data.resultCode === 0) {
+            let {id, login, email} = response.data.data;
+            dispatch(setAuthUserData(id, email, login));
+        }
+    });
+};
 
 export default authReducer;
