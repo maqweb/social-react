e8fe95092fc19db4bb90763afc0b5f097ab4ffa8
maqweb <balikhinv@bk.ru>
2020-02-07 21:58:31 +0300

    refactor app to class

--- src/App.js
+++ src/App.js
@@ -11,32 +11,34 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 
-const App = () => {
-    return (
-        <div className="mainWrap">
-            <HeaderContainer/>
-            <Navbar/>
-            <div className="content">
+class App extends React.Component<{}> {
+    render() {
+        return (
+            <div className="mainWrap">
+                <HeaderContainer/>
+                <Navbar/>
+                <div className="content">
 
-                <Route path='/profile/:userId?'
-                       render={() => <ProfileContainer/>}/>
+                    <Route path='/profile/:userId?'
+                           render={() => <ProfileContainer/>}/>
 
-                <Route path='/dialogs'
-                       render={() => <DialogsContainer/>}/>
+                    <Route path='/dialogs'
+                           render={() => <DialogsContainer/>}/>
 
-                <Route path='/news' component={News}/>
-                <Route path='/music' component={Music}/>
-                <Route path='/settings' component={Settings}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
 
-                <Route path='/users'
-                       render={() => <UsersContainer/>}/>
+                    <Route path='/users'
+                           render={() => <UsersContainer/>}/>
 
-                <Route path='/login'
-                       render={() => <Login/>}/>
+                    <Route path='/login'
+                           render={() => <Login/>}/>
 
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
